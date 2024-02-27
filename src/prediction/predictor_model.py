2d0a32af25f5c205ef8d004d8c1562d539d8cc31
rt-desai <abhyuday.desai@readytensor.com>
2024-02-26 20:49:50 -0800

    make epsilon hyperparam

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -37,6 +37,7 @@ class Forecaster:
         kernel: str = "linear",
         degree: int = 3,
         C: float = 1.0,
+        epsilon: float = 0.01,
         use_exogenous: bool = True,
         random_state: int = 0,
         **kwargs,
@@ -68,9 +69,15 @@ class Forecaster:
             degree (int):
                 Degree of the polynomial kernel function ('poly'). Must be non-negative. Ignored by all other kernels.
 
-            C (float):
+            epsilon (float):
                 Regularization parameter. The strength of the regularization is inversely proportional to C.
                 Must be strictly positive. The penalty is a squared l2 penalty.
+            
+            C (float):
+               Epsilon in the epsilon-SVR model. It specifies the epsilon-tube within which no penalty
+               is associated in the training loss function with points predicted within a distance
+               epsilon from the actual value. 
+               Must be non-negative.
 
             use_exogenous (bool): If true, uses covariates in training.
 
@@ -98,8 +105,8 @@ class Forecaster:
                 kernel=kernel,
                 degree=degree,
                 C=C,
+                epsilon=epsilon,
                 **kwargs,
-                epsilon=0.01,
             )
         ]
 
