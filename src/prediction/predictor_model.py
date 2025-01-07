81e59aa1605f5dde2e47fd456821d78d38986e60
Mo <mohamedabdelhamid3030@gmail.com>
2025-01-07 04:05:04 +0200

    Update hyperparameters to match package defaults

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -227,7 +227,7 @@ class Forecaster:
             )
 
         freq = (
-            "3W"
+            "15T"
             if self.data_schema.title.startswith("AGT Tenant")
             else self.map_frequency(self.data_schema.frequency)
         )
